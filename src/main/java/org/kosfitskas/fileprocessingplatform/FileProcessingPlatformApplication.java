1cd32caffc0a4d6c10b0c25f587ab725a110a6bb
KonstantinosK <kwstas.kosfi@gmail.com>
2026-03-16 00:10:52 +0200

    init rep with db config

---
+++ src/main/java/org/kosfitskas/fileprocessingplatform/FileProcessingPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.kosfitskas.fileprocessingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProcessingPlatformApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FileProcessingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/kosfitskas/fileprocessingplatform/FileProcessingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kosfitskas.fileprocessingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
